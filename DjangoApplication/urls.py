4e863f1ec6dff4b7442384122831a325abbc98b1
Mark Carpenter <slaughterfish@gmail.com>
2013-06-16 23:36:44 -0400

    Adding html pages, logo/favicon placeholder

--- DjangoApplication/urls.py
+++ DjangoApplication/urls.py
@@ -16,6 +16,21 @@ urlpatterns = patterns('',
     # Uncomment the next line to enable the admin:
     # url(r'^admin/', include(admin.site.urls)),
 
+    url(r'^static/(?P<path>.*)$', 'serve'),
+
+    url(r'^$', 'DjangoApplication.upboatme.home.index', name='home'),
+
+    url(r'^terms[/]$', 'DjangoApplication.upboatme.home.terms', name='terms'),
+
+    url(r'^about[/]*$', 'DjangoApplication.upboatme.home.about', name='about'),
+
+    url(r'^(?P<name>[\w-]+)/(?P<first>[ \.\-\?~!$&\'()*+,;=:@%\w]*)/?(?P<second>[ \.\-\?~!$&\'()*+,;=:@%\w]*)$',
+        'DjangoApplication.upboatme.views.make', name='make'),
+
     # this url dispatch business cannot (by design) match the querystring. So screw it, we'll do it live:
     url(r'^.*$', 'DjangoApplication.upboatme.views.route', name='route')
+)
+
+urlpatterns += patterns('django.contrib.staticfiles.views',
+    url(r'^static/(?P<path>.*)$', 'serve'),
 )
