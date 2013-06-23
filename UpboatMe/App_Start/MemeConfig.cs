f44241dabbd1159fb964d1f312ffbb65cd719b13
mharen <mharen@gmail.com>
2013-06-23 13:53:10 -0400

    added philosoraptor meme

--- UpboatMe/App_Start/MemeConfig.cs
+++ UpboatMe/App_Start/MemeConfig.cs
@@ -21,6 +21,7 @@ namespace UpboatMe.App_Start
             memes.Add(new Meme("Malicious Advice Mallard", "malicious-advice-mallard", new List<string> { "mam", "maliciousadvicemallard" }));
             memes.Add(new Meme("Overly Attached Girlfriend", "overly-attached-girlfriend", new List<string> { "oag", "overlyattachedgirlfriend" }));
             memes.Add(new Meme("Overly Manly Man", "overly-manly-man", new List<string> { "omm", "overlymanlyman" }));
+            memes.Add(new Meme("Philosoraptor", "philosoraptor", new List<string> { "raptor", "philosoraptor", "pr" }));
             memes.Add(new Meme("Scumbag Steve", "scumbag-steve", new List<string> { "ss", "scumbagsteve" }));
             memes.Add(new Meme("Socially Awesome Awkward Penguin", "socially-awesome-awkward-penguin", new List<string> { "sociallyaweomeawkwardpenguin", "awesomeawkwardpenguin" }));
             memes.Add(new Meme("Socially Awkward Awesome Penguin", "socially-awkward-awesome-penguin", new List<string> { "sociallyawkwardawesomepenguin", "awkwardawesomepenguin" }));
