33a9431a1e3597c74e5e42a154d977b6d1f4f2c0
Xander Dumaine <xander.dumaine@gmail.com>
2013-06-25 07:29:35 -0400

    One Does Not Simply (Boromir)

--- UpboatMe/App_Start/MemeConfig.cs
+++ UpboatMe/App_Start/MemeConfig.cs
@@ -31,6 +31,7 @@ namespace UpboatMe.App_Start
             memes.Add(new Meme("Irrationally Hostile Mark", "irrationally-hostile-mark", new List<string> { "irrationallyhostilemark", "hostilemark", "ihm" }));
             memes.Add(new Meme("Malicious Advice Mallard", "malicious-advice-mallard", new List<string> { "mam", "maliciousadvicemallard" }));
             memes.Add(new Meme("Office Space Lumbergh", "office-space-lumbergh", new List<string> { "osl", "lumbergh", "officespacelumbergh", "yeah" }));
+            memes.Add(new Meme("One Does Not Simply", "one-does-not-simply", new List<string> { "odns", "boromir", "mordor", "onedoesnotsimply" }));
             memes.Add(new Meme("Overly Attached Girlfriend", "overly-attached-girlfriend", new List<string> { "oag", "overlyattachedgirlfriend" }));
             memes.Add(new Meme("Overly Manly Man", "overly-manly-man", new List<string> { "omm", "overlymanlyman" }));
             memes.Add(new Meme("Philosoraptor", "philosoraptor", new List<string> { "raptor", "philosoraptor", "pr" }));
