cbfb98da27aaa4cf7fa999dc41ed21a3621ad6b6
mharen <mharen@gmail.com>
2013-06-25 22:17:00 -0400

    toy story everywhere; resolves #43

--- UpboatMe/App_Start/MemeConfig.cs
+++ UpboatMe/App_Start/MemeConfig.cs
@@ -44,7 +44,9 @@ namespace UpboatMe.App_Start
             memes.Add(new Meme("Socially Awkward Penguin", "socially-awkward-penguin", new List<string> { "sap", "sociallyawkwardpenguin" }));
             memes.Add(new Meme("Success Kid", "success-kid", new List<string> { "sk", "successkid" }));
             memes.Add(new Meme("Sudden Clarity Clarence", "sudden-clarity-clarence", new List<string> { "scc", "suddenclarityclarence" }));
+            memes.Add(new Meme("Technologically Impaired Duck", "technologically-impaired-duck", new List<string> { "tid", "technologyduck", "technologicallyimpairedduck", "technologyimpairedduck" }));
             memes.Add(new Meme("The Most Interesting Man in the World", "the-most-interesting-man-in-the-world", new List<string> { "mim", "tmim", "tmimitw", "themostinterestingmanintheworld", "mostinterestingman" }));
+            memes.Add(new Meme("Toy Story: Everywhere", "toy-story-everywhere", new List<string> { "toystory", "toystoryeverywhere", "buzzwoody" }));
             memes.Add(new Meme("Unhelpful High School Teacher", "unhelpful-high-school-teacher", new List<string>{"uhst", "unhelpfulhighschoolteacher", "scumbagteacher", "st"}));
             memes.Add(new Meme("Why Not Zoidberg?", "why-not-zoidberg", new List<string>{"wnz", "ynz", "whynotzoidberg", "zoidberg"}));
             memes.Add(new Meme("Y U NO", "y-u-no", new List<string> { "yuno" } ));
