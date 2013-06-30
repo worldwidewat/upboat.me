f1f59f39ec7d4c04a595137bccc21a4f8c123f7a
mharen <mharen@gmail.com>
2013-06-30 00:06:22 -0400

    redirect requests without an extension to add one

--- UpboatMe/Controllers/MemeController.cs
+++ UpboatMe/Controllers/MemeController.cs
@@ -1,4 +1,5 @@
-﻿using System.Linq;
+﻿using System.IO;
+using System.Linq;
 using System.Text.RegularExpressions;
 using System.Web.Mvc;
 using UpboatMe.Imaging;
@@ -10,13 +11,15 @@ namespace UpboatMe.Controllers
     public class MemeController : Controller
     {
         private static Regex _StripRegex = new Regex(@"&?drawboxes=true|\.png$|\.jpe?g$", RegexOptions.Compiled | RegexOptions.IgnoreCase);
+        private static Regex _UrlExtension = new Regex(@"\.png$|\.jpe?g$", RegexOptions.Compiled | RegexOptions.IgnoreCase);
 
         // ignore the parameters and figure it out manually from Request.RawUrl
         // this allows us to keep using routes to generate our own links, which is handy
         public ActionResult Make(string name, string top, string bottom)
         {
             var url = Request.RawUrl;
-            
+            var hasExtension = _UrlExtension.IsMatch(url);
+
             // todo - handle this more elegantly, or don't do such things via this action
             var drawBoxes = url.Contains("drawBoxes=true");
 
@@ -32,6 +35,15 @@ namespace UpboatMe.Controllers
                 meme = GlobalMemeConfiguration.NotFoundMeme;
                 memeRequest.Top = "404";
                 memeRequest.Bottom = "Y U NO USE VALID MEME NAME?";
+            }
+
+            // if we want to do this earlier in the process consider
+            // if we still need to worry about png vs. jpg. The browser
+            // probably doesn't care, but it might be weird if the extension
+            // doesn't match the mimetype
+            if (!hasExtension)
+            {
+                return Redirect(url + Path.GetExtension(meme.ImageFileName));
             }
 
             var renderParameters = new RenderParameters
