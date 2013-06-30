c152c5b063318f87dba9677b9db63dd94fe866e3
mharen <mharen@gmail.com>
2013-06-29 22:09:16 -0400

    this might be madness

---
+++ UpboatMe.Imaging.Tests/UnitTest1.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using ApprovalTests;
+using ApprovalTests.Reporters;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace UpboatMe.Imaging.Tests
+{
+    [TestClass]
+    [UseReporter(typeof(DiffReporter))]
+    public class UnitTest1
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+            Approvals.Verify("test");
+        }
+    }
+}
---
+++ UpboatMeTests/ImageIntegrationTests.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.IO;
+using ApprovalTests;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using UpboatMe.Imaging;
+using UpboatMe.Models;
+
+namespace UpboatMeTests
+{
+    [TestClass]
+    public class ImageIntegrationTests
+    {
+        [TestMethod]
+        public void TextRendererTest()
+        {
+            var filepath = Path.Combine(AppDomain.CurrentDomain.BaseDirectory, "y-u-no.jpg");
+            var meme = new Meme("test", filepath, null);
+
+            var renderParameters = new RenderParameters
+                                       {
+                                           FullImagePath = meme.ImageFileName,
+                                           TopLineHeightPercent = meme.TopLineHeightPercent,
+                                           BottomLineHeightPercent = meme.BottomLineHeightPercent,
+                                           Fill = meme.Fill,
+                                           Stroke = meme.Stroke,
+                                           Font = meme.Font,
+                                           FontSize = meme.FontSize,
+                                           StrokeWidth = meme.StrokeWidth,
+                                           DrawBoxes = false
+                                       };
+
+            var renderer = new Renderer();
+
+            var bytes = renderer.Render(renderParameters, "foo", "bar");
+
+            Approvals.Verify("test");
+        }
+
+        [TestMethod]
+        public void test()
+        {
+            Approvals.Verify("foo");
+        }
+    }
+}
