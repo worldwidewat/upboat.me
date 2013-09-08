a25d774f904b426ff16cb5ad5300ff68aa3e49fe
mharen <mharen@gmail.com>
2013-09-08 09:57:29 -0400

    encode more things in the URL

--- UpboatMe/Scripts/site.js
+++ UpboatMe/Scripts/site.js
@@ -3,9 +3,13 @@
     
     $("#builder-form").on("submit", function(e) {
         var url = $(this).attr('action');
-        url = url + "/" + $(this).find('[name=name]').val() + "/" + $(this).find('[name=top]').val() + "/" + $(this).find('[name=bottom]').val();
-        window.location.href = url;
+        url = url
+            + "/" + $(this).find('[name=name]').val()
+            + "/" + encodeURIComponent($(this).find('[name=top]').val())
+            + "/" + encodeURIComponent($(this).find('[name=bottom]').val());
+
         e.preventDefault();
+        window.location.href = url;
     });
 });
 
