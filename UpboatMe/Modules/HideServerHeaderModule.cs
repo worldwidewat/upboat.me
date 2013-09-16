ff1c87d1bad87451e2f97feb78f374e41783e830
mharen <mharen@gmail.com>
2013-09-16 12:07:26 -0400

    obfusicate the 'server' header

---
+++ UpboatMe/Modules/HideServerHeaderModule.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Web;
+
+namespace UpboatMe.Modules
+{
+    // via http://arturito.net/2011/10/21/how-to-remove-server-x-aspnet-version-x-aspnetmvc-version-and-x-powered-by-from-the-response-header-in-iis7/
+    public class HideServerHeaderModule : IHttpModule
+    {
+        public void Init(HttpApplication context)
+        {
+            context.PreSendRequestHeaders += OnPreSendRequestHeaders;
+        }
+
+        public void Dispose() { }
+
+        void OnPreSendRequestHeaders(object sender, EventArgs e)
+        {
+            HttpContext.Current.Response.Headers.Set("Server", "Memeverse/1.0");
+        }
+    }
+}
