295986c168d0e7dd12cd58e810e54ed9f8ac8665
mharen <mharen@gmail.com>
2013-09-26 22:11:28 -0400

    added simple api for getting list of memes

--- UpboatMe/App_Start/WebApiConfig.cs
+++ UpboatMe/App_Start/WebApiConfig.cs
@@ -2,6 +2,8 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Web.Http;
+using Newtonsoft.Json;
+using Newtonsoft.Json.Serialization;
 
 namespace UpboatMe
 {
@@ -14,6 +16,21 @@ namespace UpboatMe
                 routeTemplate: "api/{controller}/{id}",
                 defaults: new { id = RouteParameter.Optional }
             );
+
+            // prefer json via http://stackoverflow.com/a/12487921/29
+            var appXmlType = config.Formatters
+                                   .XmlFormatter
+                                   .SupportedMediaTypes
+                                   .FirstOrDefault(t => t.MediaType == "application/xml");
+            config.Formatters.XmlFormatter.SupportedMediaTypes.Remove(appXmlType);
+
+            // camel case and indent json output
+            var settings = GlobalConfiguration.Configuration
+                                              .Formatters
+                                              .JsonFormatter
+                                              .SerializerSettings;
+            settings.Formatting = Formatting.Indented;
+            settings.ContractResolver = new CamelCasePropertyNamesContractResolver();
         }
     }
 }
---
+++ UpboatMe/Controllers/ListController.cs
@@ -0,0 +1,46 @@
+﻿using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.Serialization;
+using System.Web.Http;
+using UpboatMe.Models;
+
+namespace UpboatMe.Controllers
+{
+    public class ListController : ApiController
+    {
+        public IEnumerable<ApiMemeResult> Get()
+        {
+            return GlobalMemeConfiguration
+                .Memes
+                .GetMemes()
+                .Select(m => new ApiMemeResult
+                {
+                    Name = m.Aliases.Last(),
+                    Description = m.Description,
+                    Aliases = m.Aliases
+                });
+        }
+
+        public ApiMemeResult Get(string id)
+        {
+            var meme = GlobalMemeConfiguration.Memes[id];
+            return new ApiMemeResult
+                       {
+                           Name = meme.Aliases.Last(),
+                           Description = meme.Description,
+                           Aliases = meme.Aliases
+                       };
+        }
+    }
+
+    [DataContract(Name = "Meme")]
+    public class ApiMemeResult
+    {
+        [DataMember]
+        public string Name { get; set; }
+        [DataMember]
+        public string Description { get; set; }
+        [DataMember]
+        public IEnumerable<string> Aliases { get; set; }
+    }
+}
